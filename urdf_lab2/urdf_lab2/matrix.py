d394cd1bd5f44828da718434678406e46eb242c6
kbugala4 <k.bugala@poczta.onet.pl>
2021-04-05 20:21:50 +0200

    komentarze w matrix

--- urdf_lab2/urdf_lab2/matrix.py
+++ urdf_lab2/urdf_lab2/matrix.py
@@ -17,28 +17,26 @@ def matrix_multiplier(a, b):
 				c[row][column] += a[row][i]*b[i][column] 
 	return c
 
-def merge_matrices(R,T):
-	H = R
-	for i in range(len(R)):
-		H[i].append(T[i])
-	last_row = [0 for i in range(len(R[0])-1)]
-	last_row.append(1)
-	H.append(last_row)
+# def merge_matrices(R,T):
+# 	H = R
+# 	for i in range(len(R)):
+# 		H[i].append(T[i])
+# 	last_row = [0 for i in range(len(R[0])-1)]
+# 	last_row.append(1)
+# 	H.append(last_row)
 
-	return H
-	
-def create_R():
-	
+# 	return H
 
-if __name__ == '__main__':
-	print('a')
-	a = [[1,2,4],
-	[3,4,6]]
 
-	b = [1,4]
+# if __name__ == '__main__':
+# 	print('a')
+# 	a = [[1,2,4],
+# 	[3,4,6]]
+
+# 	b = [1,4]
 	
-	c = merge_matrices(a,b)
-	print(c)
-	print('a')
+# 	c = merge_matrices(a,b)
+# 	print(c)
+# 	print('a')
 
 
