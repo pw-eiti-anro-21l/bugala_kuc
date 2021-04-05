cc386a5d2e4af9b9893150adaff0adcce3824ee3
kucjan <janekkuc00@gmail.com>
2021-04-05 19:56:46 +0200

    rpy

--- urdf_lab2/urdf_lab2/rpy_calculate.py
+++ urdf_lab2/urdf_lab2/rpy_calculate.py
@@ -7,8 +7,11 @@ def csv_reader(filename):
 	with open(filename, 'r') as file:
 		read_file = csv.reader(file)
 		for row in read_file:
-			DH.append(row)
+			dh.append(row)
 	return dh
 
-def rotation_matrix_calc(dh):
-	rot1 = []
+def rotation_matrix_calc(dh, filename):
+	dh = csv_reader(filename)
+	h01 = [[cos(dh[1][4]), -sin(dh[1][4])*cos(dh[1][3]), sin(dh[1][4])*sin(dh[1][3]), dh[1][1]*cos(dh[1][4])],
+		   [sin(dh[1][4]), cos(dh[1][4])*cos(dh[1][3]), -cos(dh[1][4])*sin(dh[1][3]), dh[1][1]*sin(dh[1][4])],
+		   []]
