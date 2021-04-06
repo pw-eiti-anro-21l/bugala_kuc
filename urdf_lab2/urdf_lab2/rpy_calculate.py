770b7d6673fc3790f23954bf2f1893640c072c99
kucjan <janekkuc00@gmail.com>
2021-04-06 13:00:18 +0200

    xml link

--- urdf_lab2/urdf_lab2/rpy_calculate.py
+++ urdf_lab2/urdf_lab2/rpy_calculate.py
@@ -2,6 +2,8 @@ import csv
 from math import cos, sin, atan2, sqrt
 import os
 from matrix import matrix_multiplier
+#import xml.etree.ElementTree as ET
+from lxml import etree as ET
 
 def csv_reader(filename):
 	dh = []
@@ -43,7 +45,30 @@ def find_rpy(r):
 	yaw = atan2(r[1][0], r[0][0])
 	return [roll, pitch, yaw]
 
+def link_xml_creator(r, p, y):
+	link_name = "dupa"
+	link = ET.Element('link', name = link_name)
+	inertial = ET.SubElement(link, 'inertial')
+	mass = ET.SubElement(inertial, 'mass', value = "1") 
+	inertia = ET.SubElement(inertial, 'inertia', ixx="100", ixy="0", ixz="0", iyy="100", iyz="0", izz="100")
+	origin = ET.SubElement(inertial, "origin")
+	visual = ET.SubElement(link, "visual")
+	visual_origin = ET.SubElement(visual, "origin", xyz="0 0 0", rpy="1.57 0 0")
+	geometry = ET.SubElement(visual, "geometry")
+	cylinder = ET.SubElement(geometry, "cylinder", radius="0.01", length=".5")
+	material = ET.SubElement(visual, "material", name="gray")
+	color = ET.SubElement(material, "color", rgba=".2 .2 .2 1")
+	collision = ET.SubElement(link, "collision")
+	collision_origin = ET.SubElement(collision, "origin", xyz="0 0 0", rpy="1.57 0 0")
+	collision_geometry = ET.SubElement(collision, "geometry")
+	collision_cylinder = ET.SubElement(geometry, "cylinder", radius="0.01", length=".5")
+	contact_cofficients = ET.SubElement(collision, "contact_cofficients", mu="0", kp="1000.0", kd="1.0")
+	tree = ET.ElementTree(link)
+	tree.write('robot_lab2.urdf.xml', pretty_print=True)
+
+
 if __name__ == '__main__':
 	#print(csv_reader('dh_table.csv'))
 	r = rotation_matrix_calc('dh_table.csv')
 	print(find_rpy(r))
+	link_xml_creator(1,2,3)
