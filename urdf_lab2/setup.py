18b495d6edc1dfc023770c1e1b8820db138d75c3
kucjan <janekkuc00@gmail.com>
2021-04-11 20:31:04 +0200

    nie dziala launch

--- urdf_lab2/setup.py
+++ urdf_lab2/setup.py
@@ -1,4 +1,8 @@
 from setuptools import setup
+import os
+from glob import glob
+from setuptools import setup
+from setuptools import find_packages
 
 package_name = 'urdf_lab2'
 
@@ -10,16 +14,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('urdf/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='jan',
     maintainer_email='janekkuc00@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='description',
+    license='License',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+        'state_publisher = urdf_lab2.state_publisher:main'
         ],
     },
 )
